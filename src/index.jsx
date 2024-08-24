4a52fea7ae60f157d4b576b5b4eabed4ec88086d
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2024-08-24 15:47:46 +0300

    feat: convert to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -12,25 +12,25 @@ import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 import fbConfig from "./config/fbConfig";
 
 const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reactReduxFirebase(fbConfig, {
-      //added userFirestore and userProfile to get it from the collection user
-      useFirestoreForProfile: true,
-      userProfile: "user",
-      attachAuthIsReady: true
-    }), // redux binding for firebase
-    reduxFirestore(fbConfig) // redux bindings for firestore
-  )
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reactReduxFirebase(fbConfig, {
+            //added userFirestore and userProfile to get it from the collection user
+            useFirestoreForProfile: true,
+            userProfile: "user",
+            attachAuthIsReady: true,
+        }), // redux binding for firebase
+        reduxFirestore(fbConfig) // redux bindings for firestore
+    )
 );
 //wait until the firebase ready and then load the component for security
 store.firebaseAuthIsReady.then(() => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById("root")
-  );
-  registerServiceWorker();
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    );
+    registerServiceWorker();
 });
